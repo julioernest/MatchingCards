282fa0b4626c80ed3242aef65df9e4202d0ddbf6
Julio-Ernest Costache <julioernestc@gmail.com>
2022-12-15 23:08:55 +0200

    MatchingCards - Add a button on the main scene and embed in a navigation Controller and prepare loadDate

---
+++ MatchingCards/Scenes /Main/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  MatchingCards
+//
+//  Created by Julio-Ernest Costache on 12.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var playButton: UIButton!
+    let service = PlayService()
+    var data: [Theme]?
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        setUpButtons()
+        loadData()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    func setUpButtons() {
+        playButton.layer.cornerRadius = 0.1
+    }
+    func loadData() {
+        service.getConcentrationGameThemes { [weak self] response in
+            switch response {
+            case let .success(theme):
+                self?.data = theme
+            case let .failure(error):
+                print(error)
+            }
+        }
+    }
+
+    @IBAction func didTapPlayButton(_ sender: Any) {
+        guard let data = data,
+        let nextViewController = UIStoryboard(name: "Play", bundle: nil).instantiateInitialViewController() as? PlayViewController else { return }
+        
+        var dataStore = nextViewController.router?.dataStore
+        dataStore?.theme = data.first
+        
+        navigationController?.pushViewController(nextViewController, animated: true)
+        
+       
+    }
+}
+
