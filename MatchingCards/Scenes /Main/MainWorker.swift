7a9ace64b72f9e93089f05dbfb7e56d9b40a0dd6
Julio-Ernest Costache <julioernestc@gmail.com>
2022-12-19 01:40:13 +0200

    Matching Cards - Remove the service and implement it in the worker file (refactoring to match VIPWMR arhitecture)

--- MatchingCards/Scenes /Main/MainWorker.swift
+++ MatchingCards/Scenes /Main/MainWorker.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+
+enum MainServiceErrors: Error {
+    case unableToDecodeTheJSON(Error)
+    case invalidStatusCode
+    case dataIsMissing
+    case invalidURL
+    
+}
+
+class MainWorker {
+    var webService = WebService.firebaseLink
+    
+    func loadData(completion: @escaping (Result<[Theme], Error>) -> Void) {
+            guard let url = URL(string: webService) else {
+                completion(.failure(MainServiceErrors.invalidURL))
+                return
+            }
+            
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                if let error = error {
+                    completion(.failure(error))
+                    return
+                }
+                guard (response as? HTTPURLResponse)?.statusCode == 200 else {
+                    completion(.failure(MainServiceErrors.invalidStatusCode))
+                    return
+                }
+                guard let data = data else {
+                    completion(.failure(MainServiceErrors.dataIsMissing))
+                    return }
+                do {
+                    let decodedDate = try JSONDecoder().decode([Theme].self, from: data)
+                    completion(.success(decodedDate))
+                } catch {
+                    completion(.failure(MainServiceErrors.unableToDecodeTheJSON(error)))
+                }
+            }.resume()
+    }
+}
